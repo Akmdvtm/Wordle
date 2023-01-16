1fdcc10a22e21250a55aa79c2280f262e5f90963
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-17 00:35:52 +0600

    add key listener

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script setup>
+import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import {onMounted} from "vue";
 
+const handleInput = (key) => {
+  console.log(key)
+};
+
+onMounted(() => {
+  window.addEventListener("keyup", (e) => {
+    e.preventDefault()
+    let key =
+        e.key === "Enter"
+            ? "{enter}"
+            : e.key === "Backspace"
+            ? "{bksp}"
+            : e.key.toLowerCase()
+            // : String.fromCharCode(e.code).toLowerCase()
+    handleInput(key)
+  })
+})
 </script>
 
 <template>
-  <div>
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+    <simple-keyboard @onKeyPress="handleInput"/>
   </div>
 </template>
 
