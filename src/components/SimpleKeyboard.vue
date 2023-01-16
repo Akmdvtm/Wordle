d524dea989790f17e0fa8c987e207b1fe350be5b
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-17 00:03:04 +0600

    initial commit

---
+++ src/components/SimpleKeyboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="simple-keyboard"></div>
+</template>
+
+<script setup>
+import Keyboard from 'simple-keyboard'
+import "simple-keyboard/build/css/index.css"
+import {ref, onMounted} from "vue";
+
+export default {
+
+}
+</script>
+
