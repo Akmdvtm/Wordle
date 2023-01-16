7cdfc69620c607f90ba28cf2c61debc36c438227
Tamerlan Akhmedov <98039559+Akmdvtm@users.noreply.github.com>
2023-01-17 00:18:15 +0600

    create Keyboard

--- src/components/SimpleKeyboard.vue
+++ src/components/SimpleKeyboard.vue
@@ -7,8 +7,24 @@ import Keyboard from 'simple-keyboard'
 import "simple-keyboard/build/css/index.css"
 import {ref, onMounted} from "vue";
 
-export default {
+const emit = defineEmits(["onKeyPress"]);
+const keyboard = ref(null);
 
-}
+const onKeyPress = (button) => {
+  emit("onKeyPress", button)
+};
+
+onMounted(() => {
+  Keyboard.value = new Keyboard("simple-keyboard", {
+    layout: {
+      default: [
+        "q w e r t y u i o p",
+        "a s d f g h j k l",
+        "{enter} z x c v b n m {bksp}",
+      ],
+    },
+    onKeyPress: onKeyPress,
+  });
+});
 </script>
 
